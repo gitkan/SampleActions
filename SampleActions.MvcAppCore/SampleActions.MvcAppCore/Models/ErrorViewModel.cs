cd462e6528ce8fd2515e0fa021a58b1b0df5a6a4
Kan, Seijiro <skan@jbs.com>
2022-01-30 11:22:23 +0900

    new sample appication

---
+++ SampleActions.MvcAppCore/SampleActions.MvcAppCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleActions.MvcAppCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
